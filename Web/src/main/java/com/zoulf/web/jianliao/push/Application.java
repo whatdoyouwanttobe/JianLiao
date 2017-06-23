fb4493fe5803c6c5d69ba80f52bc146170bbdf2e
15128452559@163.com <15128452559@163.com>
2017-06-23 16:11:30 +0800

    后台注册接口编写

--- Web/src/main/java/com/zoulf/web/jianliao/push/Application.java
+++ Web/src/main/java/com/zoulf/web/jianliao/push/Application.java
@@ -1,21 +1,24 @@
 package com.zoulf.web.jianliao.push;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import com.zoulf.web.jianliao.push.provider.GsonProvider;
 import com.zoulf.web.jianliao.push.service.AccountService;
 import java.util.logging.Logger;
 import org.glassfish.jersey.server.ResourceConfig;
 
 /**
- * @author  Zoulf
+ * @author Zoulf
  */
-public class Application extends ResourceConfig{
+public class Application extends ResourceConfig {
 
   public Application() {
     // 注册逻辑处理的包名
     packages(AccountService.class.getPackage().getName());
 
     // 注册Json解析器
-    register(JacksonJsonProvider.class);
+    // register(JacksonJsonProvider.class);
+    // 替换为Gson解析器
+    register(GsonProvider.class);
 
     // 注册日志打印输出
     register(Logger.class);
